7288aeceeeb27a46f04043b5f1417e44b0e40466
Bianca Farias <bifarias202@gmail.com>
2025-09-10 14:48:20 +0000

    Adiciona conteúdo do aula 26

---
+++ secao2-Revisao-de-logica/26-exemplo-logico.js
@@ -0,0 +1,9 @@
+let idade = 18
+
+let paisPresentes = false
+
+let comprouBilhete = false
+
+const podeViajar = (idade >= 18 || paisPresentes) && comprouBilhete
+
+console.log(`Pode viajar? ${podeViajar}`)
