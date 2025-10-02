058e63ed75797d7f2565de1b1474e142712256cd
Bianca Farias <bifarias202@gmail.com>
2025-10-02 21:06:35 +0000

    Adiciona melhorias ao exercicio 37

--- secao2-Revisao-de-logica/37-execicio/script.js
+++ secao2-Revisao-de-logica/37-execicio/script.js
@@ -1,24 +1,23 @@
 const output = document.querySelector("#output")
 const input = document.querySelector("#input")
+const table = document.createElement("table")
 
 function mostrarTabuada() {
+    limparTabuada()
     // output.innerHTML = "Valor qualquer <br> pula linha"
     // pedir para o usuário digitar um número
     let num = Number.parseInt(input.value)
 
-    const table = document.createElement("table")
     output.appendChild(table)
-    let linha = document.createElement("tr")
     let html = ""
     // mostrar a tabuada desse número de 1 a 1000
     for (let i = 0; i <= 1000; i++) {
         let result = num * i
 
-        html += `${num} x ${i} = ${result}<br>`
+        html += `<tr><td>${num} x ${i} = ${result}</td></tr>`
     }
-    linha.innerHTML = html
+    table.innerHTML = html
     // Toda informação que vem da interface do usuário  é string
-    table.appendChild(linha)
 }
 
 function limparTabuada() {
