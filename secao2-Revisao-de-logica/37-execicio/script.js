f5108c9edc00143120e02ea405ab29433fccf7ec
Bianca Farias <bifarias202@gmail.com>
2025-10-02 20:57:05 +0000

    Adiciona estilos simples

--- secao2-Revisao-de-logica/37-execicio/script.js
+++ secao2-Revisao-de-logica/37-execicio/script.js
@@ -9,14 +9,18 @@ function mostrarTabuada() {
     const table = document.createElement("table")
     output.appendChild(table)
     let linha = document.createElement("tr")
-
+    let html = ""
     // mostrar a tabuada desse número de 1 a 1000
     for (let i = 0; i <= 1000; i++) {
         let result = num * i
 
-        linha.innerHTML += `${num} x ${i} = ${result}<br>`
+        html += `${num} x ${i} = ${result}<br>`
     }
-
+    linha.innerHTML = html
     // Toda informação que vem da interface do usuário  é string
     table.appendChild(linha)
+}
+
+function limparTabuada() {
+    output.innerHTML = ""
 }
