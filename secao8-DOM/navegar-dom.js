3a73571a43a911e0896d9d245246692634b9fe7b
Bianca Farias <bifarias202@gmail.com>
2025-12-04 23:44:30 +0000

    Adiciona ´aula 110´

---
+++ secao8-DOM/navegar-dom.js
@@ -0,0 +1,13 @@
+
+const nodeEl = document.getElementById("list")
+
+// Métodos para navegar entre os Nodes
+
+/*
+    Node.parentNode
+    Node.parentElement
+    Node.nextSibling
+    Node.previousSibling
+ */
+
+console.log(nodeEl.parentNode)
